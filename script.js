e5f3206345cd5601c7a5d40009d6c3570b9443b5
mhelenaz <souza.silva.maria2003@escola.pr.gov.br>
2025-07-02 09:46:43 -0300

    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Adicionar interatividade, se necessário
+
+// Exemplo: Quando o documento for carregado, mostrar uma mensagem simples no console.
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Bem-vindo à Festa Junina da Escola São João!");
+});
